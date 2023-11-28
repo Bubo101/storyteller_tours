2f30cade3bd68766c0d4d176bb918997be71b08a
Boden <boden.bradley@gmail.com>
2023-11-28 10:00:48 -0800

    updated models to include name fields, ran migrations, added str meta functions

--- _ToursAPP/models.py
+++ _ToursAPP/models.py
@@ -19,6 +19,7 @@ class Tour(models.Model):
 
 
 class Stop(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
     tour = models.ForeignKey(Tour, on_delete=models.CASCADE, related_name="stops")
     location = models.CharField(max_length=255)
     description = models.TextField(default="")
@@ -27,13 +28,31 @@ class Stop(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
 
+    def save(self, *args, **kwargs):
+        # Check if the name is not set
+        if not self.name:
+            # Set the default value as the object id
+            self.name = f"Stop-{self.id}"
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return self.location
+        return self.name
 
 
 class SubStop(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
     stop = models.ForeignKey(Stop, on_delete=models.CASCADE, related_name="sub_stop")
     order = models.SmallIntegerField()
+
+    def save(self, *args, **kwargs):
+        # Check if the name is not set
+        if not self.name:
+            # Set the default value as the object id
+            self.name = f"Stop-{self.id}"
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 
 # class Purchase(models.Model):
